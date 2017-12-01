ad239d790cbf6ec537c325ee0a50eda0caa85c05
mikism98 <miguelangelsm98@hotmail.com>
2017-12-01 17:47:25 +0100

    impossible

---
+++ impossible.h
@@ -0,0 +1,27 @@
+#include "character.h"
+#include "image.h"
+#include "impossible.h"
+
+
+thread_function_move_recta1(character* mChar);
+
+thread_function_move_recta2(character* mChar);
+
+thread_function_move_cuadrado1(character* mChar);
+
+thread_function_move_cuadrado2(character* mChar);
+
+thread_function_move_cuadrado3(character* mChar);
+
+thread_function_move_diagonal1(character* mChar);
+
+thread_function_move_diagonal2(character* mChar);
+
+int impossible_level_1(character* mChar, character* mChar1, character* mChar2);
+
+int impossible_level_2(character* mChar, character* mChar1, character* mChar2);
+
+int impossible_level_3(character* mChar, character* mChar1, character* mChar2, character* mChar3);
+
+int impossible_level_4(character* mChar, character* mChar1, character* mChar2, character* mChar3, character* mChar4, character* mChar5);
+
