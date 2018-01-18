130fc879fb93cbe1007d83847e16234e9e53b14f
JavierLopezC <javier.lopezcano@estudiante.uam.es>
2018-01-18 05:41:18 +0100

    exchanges and questions

--- character.c
+++ character.c
@@ -1,7 +1,6 @@
 #include "character.h"
 #include "image.h"
 #include "nprint.h"
-#include "place.h"
 #include <string.h>
 #include <stdlib.h>
 #define CYAN_BG 46
@@ -10,16 +9,18 @@ struct _Character{
 	int type;
 	char* name;
 	Image* image;
-	object* obj;
-	/*they have only 1 object so it's object* and not object** */
+	object** obj;
+	/*they can have up to 10 objects it ends with NULL*/
 	/*if it has no object this field is NULL*/
 	object* needs;
 	/*they have only 1 object needed so it's object* and not object** */
 	/*if it has no object needed this field is NULL*/
 	char* info;	
+	resource** res;
+	/*it ends with NULL*/
 };
 
-character* iniCharacter(char *name, Image *image, int type, object* obj, object* needs, char* info){
+character* iniCharacter(char *name, Image *image, int type, object** obj, object* needs, char* info, resource** res){
 	character* mChar;
 	if(name == NULL || image == NULL) return NULL;
 
@@ -51,6 +52,7 @@ character* iniCharacter(char *name, Image *image, int type, object* obj, object*
 	mChar->needs = needs;
 	mChar->obj = obj;
 	mChar->type = type;
+	mChar->res = res;
 	return mChar;
 }
 
@@ -78,11 +80,9 @@ Status changeCharLocation(character *mChar, int x, int y){
 	return OK;
 }
 
-Status moveChar(int x, int y, character *mChar){
+PlaceAvailable moveChar(int x, int y, character *mChar){
 	if(mChar == NULL) return ERROR;
-	imageMove(mChar->image, x, y);
-
-	return OK;
+	return imageMove(mChar->image, x, y);
 }
 
 
@@ -107,9 +107,55 @@ int charGetType(character* mChar){
 	return mChar->type;
 }
 
-Status charSetObject(character* mChar, object* obj){
+Status charSetRes(character* mChar, resource** res){
+	if(mChar==NULL) return ERROR;
+	mChar->res=res;
+	return OK;
+}
+
+resource** charGetRes(character* mChar){
+	if(mChar==NULL) return NULL;
+	return mChar->res;
+}
+
+Status charSetObject(character* mChar, object** obj){
 	if(mChar==NULL) return ERROR;
 	mChar->obj=obj;
+	return OK;
+}
+
+Status charAddObject(character* mChar, object* obj){
+	if(mChar==NULL||obj==NULL) return ERROR;
+
+	object** array;
+	int i;
+
+	array=charGetObject(mChar);
+	for(i=0; i<10; i++){
+		if(array[i]==NULL){
+			array[i]=obj;
+			array[i+1]=NULL;
+			break;
+		}
+	}
+	return OK;
+}
+
+Status charDropObject(character* mChar, object* obj){
+	if(mChar==NULL||obj==NULL) return ERROR;
+
+	object** array;
+	int i, f=0;
+
+	array=charGetObject(mChar);
+	for(i=0; array[i]!=NULL; i++){
+		if(array[i]==obj){
+			f=1;
+		}
+		if(f==1){
+			array[i]=array[i+1];
+		}
+	}
 	return OK;
 }
 
@@ -119,7 +165,7 @@ Status charSetNeeds(character* mChar, object* obj){
 	return OK;
 }
 
-object* charGetObject(character* mChar){
+object** charGetObject(character* mChar){
 	if(mChar==NULL) return NULL;
 	return mChar->obj;
 }
@@ -145,12 +191,25 @@ int charGetY(character* mChar){
 	return getImageY(mChar->image);
 }
 
+Image* charGetImage(character* mChar){
+	if(mChar==NULL) return NULL;
+	return mChar->image;
+}
+
+Status charSetImage(character* mChar, Image* image){
+	if(mChar==NULL||image==NULL) return ERROR;
+	mChar->image=image;
+	return OK;
+}
+
 character* charGetNextTo(character** chars){
 	if(chars==NULL) return NULL;
 
 	int i;
+	Position result;
 	for(i=1; chars[i]!=NULL; i++){
-		if(imagesNear(chars[0]->image, chars[i]->image)==1||imagesNear(chars[0]->image, chars[i]->image)==2) return chars[i];
+		result=imagesNear(chars[0]->image, chars[i]->image);
+		if(result==NEAR||result==INSIDE) return chars[i];
 	}
 	return NULL;
 }
@@ -162,16 +221,39 @@ Status charPrintInfo(character** chars, Place* place){
 	
 	mChar=charGetNextTo(chars);
 	if(mChar==NULL){
-		printInsidePlace(place, "No hay nadie al lado.\n", placeGetFgColor(place));
+		printInsidePlace(place, "There is noone near to talk to..\n", placeGetFgColor(place));
 		return OK;
 	}
 	s=charGetInfo(mChar);
 	if(s==NULL) return ERROR;
 	if(strcmp(s,"cero")==0){
-		printInsidePlace(place, "No tiene nada que decir.\n",placeGetFgColor(place));
+		printInsidePlace(place, "I have nothing to say.\n",placeGetFgColor(place));
 		return OK;
 	}
 	printInsidePlace(place, s, placeGetFgColor(place));
+	return OK;
+}
+
+Status charPrintInfo2(character* mChar, Place* place){
+	char* s;
+	if(mChar==NULL) return ERROR;
+
+	s=charGetInfo(mChar);
+	if(s==NULL) return ERROR;
+	if(strcmp(s,"cero")==0){
+		printInsidePlace(place, "I have nothing to say.\n",placeGetFgColor(place));
+		return OK;
+	}
+	printInsidePlace(place, s, placeGetFgColor(place));
+	return OK;
+}
+
+Status charFreeCharacters(character** chars){
+	if(chars==NULL) return ERROR;
+	int i;
+	for(i=1; chars[i]; i++){
+		freeCharacter(chars[i]);
+	}
 	return OK;
 }
 
--- character.h
+++ character.h
@@ -4,16 +4,18 @@
 #include "types.h"
 #include "objects.h"
 #include "image.h"
+#include "resources.h"
+#include "place.h"
 
 typedef struct _Character character;
 
-character* iniCharacter(char *name, Image *image, int type, object* obj, object* needs, char* info);
+character* iniCharacter(char *name, Image *image, int type, object** obj, object* needs, char* info, resource** res);
 
 Status freeCharacter(character *mChar);
 
 Status changeCharLocation(character *mChar, int lat, int lng);
 
-Status moveChar(int lat, int lng, character *mChar);
+PlaceAvailable moveChar(int lat, int lng, character *mChar);
 
 Status charChangeName(character* mChar, char* name);
 
@@ -21,11 +23,19 @@ char* charGetName(character* mChar);
 
 int charGetType(character* mChar);
 
-Status charSetObject(character* mChar, object* obj);
+Status charSetObject(character* mChar, object** obj);
+
+Status charAddObject(character* mChar, object* obj);
+
+Status charDropObject(character* mChar, object* obj);
 
 Status charSetNeeds(character* mChar, object* obj);
 
-object* charGetObject(character* mChar);
+Status charSetRes(character* mChar, resource** res);
+
+resource** charGetRes(character* mChar);
+
+object** charGetObject(character* mChar);
 
 object* charGetNeeds(character* mChar);
 
@@ -35,9 +45,17 @@ int charGetX(character* mChar);
 
 int charGetY(character* mChar);
 
+Status charSetImage(character* mChar, Image* image);
+
+Image* charGetImage(character* mChar);
+
 character* charGetNextTo(character** chars);
 
 Status charPrintInfo(character** chars, Place* place);
+
+Status charPrintInfo2(character* mChar, Place* place);
+
+Status charFreeCharacters(character** chars);
 
 /*character** getCharactersFromFile(char* file, object** objects);*/
 
